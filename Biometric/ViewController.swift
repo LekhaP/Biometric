fda3604149c30fa6bf51909ea3a276af6d91ab13
preeti lekha <preeti@cantina.co>
2021-06-01 14:26:13 -0500

    Biometrics — Face ID or Touch ID
    Explore LAContext for biometric login.

--- Biometric/ViewController.swift
+++ Biometric/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func authenticateTapped(_ sender: Any) {
+        
+        let context = LAContext()
+        var error: NSError?
+        var unlockSecretMessage = "Hello!! You are in"
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            let reason = "Identify yourself"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason, reply: {success, evaluateError in
+                DispatchQueue.main.async {
+                    if success {
+                        DispatchQueue.main.async {
+                            let ac = UIAlertController(title: "Biometry complete", message: "Your device is now authentication.", preferredStyle: .alert)
+                            ac.addAction(UIAlertAction(title: "OK", style: .default))
+                            self.present(ac, animated: true)
+                        }
+                    } else {
+                        var message: String
+                        switch evaluateError {
+                        case LAError.authenticationFailed?:
+                            message = "There was a problem verifying your identity."
+                        case LAError.userCancel?:
+                            message = "You pressed cancel."
+                        case LAError.userFallback?:
+                            message = "You pressed password."
+                        case LAError.biometryNotAvailable?:
+                            message = "Face ID/Touch ID is not available."
+                        case LAError.biometryNotEnrolled?:
+                            message = "Face ID/Touch ID is not set up."
+                        case LAError.biometryLockout?:
+                            message = "Face ID/Touch ID is locked."
+                        default:
+                            message = "Face ID/Touch ID may not be configured"
+                        }
+                        
+                        let ac = UIAlertController(title: "Authentication failed", message: message, preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self.present(ac, animated: true)
+                    }
+                }
+            })
+            
+        } else {
+            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
+        }
+    }
 }
-
